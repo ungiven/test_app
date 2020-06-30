5255d097044c74245c4264ad4fa7afdd9dca16fc
ungiven <per.boda@gmail.com>
2020-06-30 23:33:22 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { Text, View, Button, Image } from "react-native";
+import { Text, View, Button, Image, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { render } from "react-dom";
 
 const Stack = createStackNavigator();
 const navEntries = ["Hund", "Katt", "Kanin"];
+
+// const NavBar = (props) => {
+//   return (
+//     <View
+//       style={{
+//         flexDirection: "row",
+//         justifyContent: "space-between",
+//         backgroundColor: props.bg,
+//       }}
+//     >
+//       <Button
+//         title="Hund"
+//         color="pink"
+//         onPress={() => props.arg.navigate("Hund")}
+//       />
+//       <Button
+//         title="Katt"
+//         color="lightblue"
+//         onPress={() => props.arg.navigate("Katt")}
+//       />
+//       <Button
+//         title="Kanin"
+//         color="lightgreen"
+//         onPress={() => props.arg.navigate("Kanin")}
+//       />
+//     </View>
+//   );
+// };
+
+const styles = StyleSheet.create({
+  button: {
+    textAlign: "center",
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
+    borderColor: "#888",
+    flex: 1,
+  },
+});
 
 const NavBar = (props) => {
   return (
@@ -17,32 +56,36 @@ const NavBar = (props) => {
         backgroundColor: props.bg,
       }}
     >
-      <Button
-        title="Hund"
-        color="pink"
+      <Text
+        style={[styles.button, { backgroundColor: "pink" }]}
         onPress={() => props.arg.navigate("Hund")}
-      />
-      <Button
-        title="Katt"
-        color="lightblue"
+      >
+        Hund
+      </Text>
+      <Text
+        style={[styles.button, { backgroundColor: "lightblue" }]}
         onPress={() => props.arg.navigate("Katt")}
-      />
-      <Button
-        title="Kanin"
-        color="lightgreen"
+      >
+        Katt
+      </Text>
+      <Text
+        style={[styles.button, { backgroundColor: "palegreen" }]}
         onPress={() => props.arg.navigate("Kanin")}
-      />
+      >
+        Kanin
+      </Text>
     </View>
   );
 };
 
 const screenDog = ({ navigation }) => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <NavBar arg={navigation} bg="#eee" />
       <View
         style={{
-          height: 500,
+          flex: 1,
+          //height: 500,
           backgroundColor: "pink",
           justifyContent: "center",
           alignItems: "center",
@@ -59,11 +102,12 @@ const screenDog = ({ navigation }) => {
 
 const screenCat = ({ navigation }) => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <NavBar arg={navigation} bg="#eee" />
       <View
         style={{
-          height: 500,
+          flex: 1,
+          //height: 500,
           backgroundColor: "lightblue",
           justifyContent: "center",
           alignItems: "center",
@@ -80,11 +124,12 @@ const screenCat = ({ navigation }) => {
 
 const screenRabbit = ({ navigation }) => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <NavBar arg={navigation} bg="#eee" />
       <View
         style={{
-          height: 500,
+          flex: 1,
+          //height: 500,
           backgroundColor: "palegreen",
           justifyContent: "center",
           alignItems: "center",
