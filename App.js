3182c1ff57ae7c8603ce413764aaa9465a0c82fd
PB <per.boda@gmail.com>
2020-07-01 22:34:26 +0200

    dynamisk hantering av komponenter

--- App.js
+++ App.js
@@ -6,35 +6,23 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { render } from "react-dom";
 
 const Stack = createStackNavigator();
-const navEntries = ["Hund", "Katt", "Kanin"];
+const Entries = [
+  { name: "Hund", color: "pink" },
+  { name: "Katt", color: "lightblue" },
+  { name: "Kanin", color: "palegreen" },
+  { name: "Ekorre", color: "lightyellow" },
+];
 
-// const NavBar = (props) => {
-//   return (
-//     <View
-//       style={{
-//         flexDirection: "row",
-//         justifyContent: "space-between",
-//         backgroundColor: props.bg,
-//       }}
-//     >
-//       <Button
-//         title="Hund"
-//         color="pink"
-//         onPress={() => props.arg.navigate("Hund")}
-//       />
-//       <Button
-//         title="Katt"
-//         color="lightblue"
-//         onPress={() => props.arg.navigate("Katt")}
-//       />
-//       <Button
-//         title="Kanin"
-//         color="lightgreen"
-//         onPress={() => props.arg.navigate("Kanin")}
-//       />
-//     </View>
-//   );
-// };
+const Images = [
+  require("./assets/app_img/dog.jpg"),
+  require("./assets/app_img/cat.jpg"),
+  require("./assets/app_img/bunny.jpg"),
+  require("./assets/app_img/squirrel.jpg"),
+];
+
+const TextButtons = [];
+const Screens = [];
+const Stacks = [];
 
 // props: color, order, name
 const TextButton = (props) => {
@@ -67,7 +55,33 @@ const styles = StyleSheet.create({
 });
 
 const NavBar = (props) => {
+  TextButtons.length = 0;
+
+  for (const [index, entry] of Entries.entries()) {
+    TextButtons.push(
+      <TextButton
+        color={entry.color}
+        active={props.active}
+        order={index + 1}
+        name={entry.name}
+        arg={props.arg}
+        key={index}
+      />
+    );
+  }
+
   return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      {TextButtons}
+    </View>
+  );
+
+  /*return (
     <View
       style={{
         flexDirection: "row",
@@ -103,10 +117,35 @@ const NavBar = (props) => {
         arg={props.arg}
       />
     </View>
-  );
+  );*/
 };
 
-const screenDog = ({ navigation }) => {
+for (const [index, entry] of Entries.entries()) {
+  Screens.push(({ navigation }) => {
+    return (
+      <View style={{ flex: 1 }}>
+        <NavBar arg={navigation} active={index + 1} />
+        <View
+          style={{
+            flex: 1,
+            //height: 500,
+            backgroundColor: entry.color,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image source={Images[index]} style={{ height: 300, width: 300 }} />
+        </View>
+      </View>
+    );
+  });
+
+  Stacks.push(
+    <Stack.Screen name={entry.name} component={Screens[index]} key={index} />
+  );
+}
+
+/*const screenDog = ({ navigation }) => {
   return (
     <View style={{ flex: 1 }}>
       <NavBar arg={navigation} active="1" />
@@ -187,17 +226,25 @@ const screenSquirrel = ({ navigation }) => {
       ></View>
     </View>
   );
-};
+};*/
+
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Hund" component={Screens[0]} />
+//         <Stack.Screen name="Katt" component={Screens[1]} />
+//         <Stack.Screen name="Kanin" component={Screens[2]} />
+//         <Stack.Screen name="Ekorre" component={Screens[3]} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Hund" component={screenDog} />
-        <Stack.Screen name="Katt" component={screenCat} />
-        <Stack.Screen name="Kanin" component={screenRabbit} />
-        <Stack.Screen name="Ekorre" component={screenSquirrel} />
-      </Stack.Navigator>
+      <Stack.Navigator>{Stacks}</Stack.Navigator>
     </NavigationContainer>
   );
 };
